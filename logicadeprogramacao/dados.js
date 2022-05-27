87d1e59c9435ab7ed48b92311fe7f5c1a7028ee3
Thailan <thailanmendes88@gmail.com>
2022-05-27 10:34:40 -0300

    logica de programacao

---
+++ logicadeprogramacao/dados.js
@@ -0,0 +1,12 @@
+var nome = ['joão','maria','ana']
+var estado = ['Sao Paulo', 'Rj' , 'Bahia']
+var tel = [42213373 , 967610332, 32451207]
+console.log(nome)
+console.log(estado)
+console.log(tel)
+
+
+
+
+
+
